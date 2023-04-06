1fe576781793e2fae0cff444332c8bf542063005
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-06 14:29:51 +0700

    Add project files.

---
+++ Easd_1_106_C/Easd_1_106_C.cpp
@@ -0,0 +1,15 @@
+//1.dikarenakan dengan menggunakan algoritma masalah akan terpecahkan secara berurutan, tertahap dan lengkap satu persatu tidak ada langkah yang tertinggal
+//2.	 a. Data struktur Linier
+//		 b. Data Struktur  in Linier 
+//3.	 a. Operator
+//		 b. Banyak Langkah
+//		 c. Besar dan jenis input data
+//4.Jenis Algoritma insertionSort, Algoritma ini sangat cepat untuk data kecil. Pengurutan dilakukan dengan membandingkan setiap elemen satu per satu dan memasukkannya ke posisi yang benar secara berurutan.
+//5.Algoritma ini sangat cepat untuk data kecil. Pengurutan dilakukan dengan membandingkan setiap elemen satu per satu dan memasukkannya ke posisi yang benar secara berurutan.
+//6.
+#include<iostream>
+using namespace std;
+
+int arr[
+int cmp_count = 0;
+int mov_count = [26];
